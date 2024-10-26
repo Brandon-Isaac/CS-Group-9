3869077c8fe21ca41a0a0dc4b2447c15fa87496e
Brandon-Isaac <isaac.datch22@students.dkut.ac.ke>
2024-10-26 09:09:39 +0300

    .

--- run_zara.py
+++ run_zara.py
@@ -1,5 +1,4 @@
 from zara_parser import parser
-
 # Tests
 test1 = "3 + 4 * 5"
 test2 = "IF x THEN x = x + 1 ELSE x = x - 1 END"
@@ -8,4 +7,3 @@ test3 = "WHILE x < 10 DO x = x + 1 END"
 print(parser.parse(test1))
 print(parser.parse(test2))
 print(parser.parse(test3))
-
